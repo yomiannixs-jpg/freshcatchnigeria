bc23af47672a7f766a0e9c467f4e64cec0a4ba46
yomiannixs-jpg <yomiannixs@gmail.com>
2026-04-01 16:06:37 -0700

    Fix supabaseUrl formatting in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  supabaseUrl: "https:// rwfcgjhgenoicvarpzze.supabase.co",
+  supabaseUrl: "https://rwfcgjhgenoicvarpzze.supabase.co",
   supabaseAnonKey: "sb_publishable_87HpptbyngKRTrw2o6lqFg_bL8A_jwn",
   adminRedirectAfterLogin: "./admin.html"
 };
